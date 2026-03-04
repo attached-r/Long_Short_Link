f7e5289ee20dd3d75aa8dea8dd522fe26285353a
attached <2165329383@qq.com>
2026-03-04 13:16:31 +0800

    2026/3/4 初始化项目仓库

---
+++ Backend/highlink/src/main/java/rj/highlink/HighlinkApplication.java
@@ -0,0 +1,13 @@
+package rj.highlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HighlinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HighlinkApplication.class, args);
+    }
+
+}
---
+++ Backend/highlink/src/test/java/rj/highlink/HighlinkApplicationTests.java
@@ -0,0 +1,13 @@
+package rj.highlink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HighlinkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
